d4f4dd3824c9d139bec7632eaec31f58f198d0cc
florescon <w-upx@live.com>
2023-03-30 12:44:39 -0600

    mar 30

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -28,54 +28,6 @@
     {{-- Strawberry --}}
     {{-- <link rel="stylesheet" href="{{ asset('css_custom/strawberry.css') }}"> --}}
 
-    <style type="text/css">
-        
-        sup {color:red;}
-
-        .pagination {
-            flex-wrap: wrap;
-        }
-
-        .select2-search--inline {
-            display: contents; /*this will make the container disappear, making the child the one who sets the width of the element*/
-        }
-
-        .select2-search__field:placeholder-shown {
-            width: 100% !important; /*makes the placeholder to be 100% of the width while there are no options selected*/
-        }
-
-        .box-color {
-          height: 13px;
-          width: 13px;
-          border: 1px solid black;
-        }
-
-        .box-color-discrete {
-          height: 8px;
-          width: 8px;
-        }
-
-        .layout-switcher { position: fixed; bottom: 0; left: 50%; transform: translateX(-50%) translateY(73px); color: #fff; transition: all .35s ease; background: #343a40; border-radius: .25rem .25rem 0 0; padding: .75rem; z-index: 999; }
-        .layout-switcher:not(:hover){ opacity: .95; }
-        .layout-switcher:not(:focus){ cursor: pointer; }
-        .layout-switcher-head{ font-size: .75rem; font-weight: 600; text-transform: uppercase; }
-        .layout-switcher-head i{ font-size: 1.25rem; transition: all .35s ease; }
-        .layout-switcher-body{ transition: all .55s ease; opacity: 0; padding-top: .75rem; transform: translateY(24px); text-align: center; }
-        .layout-switcher:focus{ opacity: 1; outline: none; transform: translateX(-50%) translateY(0); }
-        .layout-switcher:focus .layout-switcher-head i{ transform: rotateZ(180deg); opacity: 0; }
-        .layout-switcher:focus .layout-switcher-body{ opacity: 1; transform: translateY(0); }
-        .layout-switcher-option{ width: 72px; padding: .25rem; border: 2px solid rgba(255,255,255,0); display: inline-block; border-radius: 4px; transition: all .35s ease; }
-        .layout-switcher-option.active{ border-color: white; text-decoration: none; }
-
-        .layout-switcher-option.active a:hover{ border-color: white; text-decoration: none; }
-
-        .layout-switcher-icon{ width: 100%; border-radius: 4px; }
-        .layout-switcher-body:hover .layout-switcher-option:not(:hover){ opacity: .5; transform: scale(0.9); }
-        @media all and (max-width: 990px){ .layout-switcher{ min-width: 250px; } }
-        @media all and (max-width: 767px){ .layout-switcher{ display: none; } }
-
-    </style>
-
     {{-- <link rel="stylesheet" href="{{ asset('css_custom/app/all.min.css') }}" /> --}}
     <link href="{{ asset('css_custom/app/filepond.css') }}" rel="stylesheet">
     <link href="{{ asset('css_custom/app/filepond-plugin-image-preview.css') }}" rel="stylesheet">
